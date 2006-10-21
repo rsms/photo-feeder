7fc6acd74795ef1de64b14f3fe17d58a92a1d8d0
rasmus <rasmus@98834e0e-571d-0410-bf09-ac0e1acfc769>
2006-10-21 20:57:24 +0000

    FlickrProvider now gets images live from flicks
    git-svn-id: http://svn.hunch.se/photo-feeder/trunk@12 98834e0e-571d-0410-bf09-ac0e1acfc769

--- PFFlickrProvider.h
+++ PFFlickrProvider.h
@@ -1,5 +1,8 @@
 #import "PFThreadedProvider.h"
+//#import <ObjectiveFlickr/ObjectiveFlickr.h>
 
 @interface PFFlickrProvider : PFThreadedProvider {
+	//OFFlickrContext*    flickrContext;
+	//OFFlickrInvocation* flickrInvoc;
 }
 @end
--- PFFlickrProvider.m
+++ PFFlickrProvider.m
@@ -5,23 +5,48 @@
 -(id)init
 {
 	[super init];
-	// TODO:
-	// Create the OFFlickrContext to store your API key and use OFFliickrInvocation to call any Flickr API method
+	
+	//flickrContext = [[OFFlickrContext contextWithAPIKey:OFDemoAPIKey sharedSecret:OFDemoSharedSecret] retain];
+	//flickrInvoc = [[OFFlickrInvocation invocationWithContext:flickrContext delegate:self] retain];
+	
 	return self;
 }
 
 
 -(void)addURLs
-{
-	// get some images from flickr
-		// TODO: per_page = buffer->capacity
-	/*url = [NSURL URLWithString:@"http://api.flickr.com/services/rest/?method=flickr.favorites.getPublicList&api_key=9b4439ce94de7e2ec2c2e6ffadc22bcf&user_id=12281432@N00&per_page=20"];
-	NSXMLDocument* doc = [[NSXMLDocument alloc] initWithContentsOfURL:url options:0 error:nil];*/
+{	
+	NSURL* url = [NSURL URLWithString:@"http://api.flickr.com/services/rest/?method=flickr.favorites.getPublicList&api_key=9b4439ce94de7e2ec2c2e6ffadc22bcf&user_id=12281432@N00&per_page=999&extras=date_taken"];
+	NSXMLDocument* doc = [[[NSXMLDocument alloc] initWithContentsOfURL:url options:0 error:nil] autorelease];
 	
-	[urls addObject:[NSURL URLWithString:@"http://static.flickr.com/79/270773771_43fea70d2b_b.jpg"]];
-	[urls addObject:[NSURL URLWithString:@"http://static.flickr.com/90/245707482_620b878566_b.jpg"]];
-	[urls addObject:[NSURL URLWithString:@"http://static.flickr.com/96/207296186_07c83ed2fa_b.jpg"]];
-	[urls addObject:[NSURL URLWithString:@"http://static.flickr.com/90/245707482_620b878566_b.jpg"]];
+	// Flickr service error?
+	if([[[[doc rootElement] attributeForName:@"stat"] stringValue] compare:@"fail" options:0] == 0)
+	{
+		throw_ex(@"PFFlickrProviderException", [[(NSXMLElement*)[[doc rootElement] childAtIndex:0] attributeForName:@"msg"] stringValue]);
+	}
+	
+	// Find photos
+	NSError* err = nil;
+	NSArray* childs = [[doc rootElement] nodesForXPath:@"/rsp/photos/photo" error:&err];
+	if(err) // TODO: test and fix
+		NSLog(@"Error: %@", err);
+	
+	// Add URLs
+	NSEnumerator* it = [childs objectEnumerator];
+	NSXMLElement* n;
+	while (n = (NSXMLElement*)[it nextObject]) {
+		NSString* urlString = [[NSString stringWithFormat:@"http://static.flickr.com/%@/%@_%@_b.jpg",
+			[[n attributeForName:@"server"] stringValue],
+			[[n attributeForName:@"id"] stringValue],
+			[[n attributeForName:@"secret"] stringValue]] autorelease];
+		NSLog(@"url: %@", urlString);
+		[urls addObject:[NSURL URLWithString:urlString]];
+	}
+	
+	// Sure shots:
+	//[urls addObject:[NSURL URLWithString:@"http://static.flickr.com/79/270773771_43fea70d2b_b.jpg"]];
+	//[urls addObject:[NSURL URLWithString:@"http://static.flickr.com/90/245707482_620b878566_b.jpg"]];
+	//[urls addObject:[NSURL URLWithString:@"http://static.flickr.com/96/207296186_07c83ed2fa_b.jpg"]];
+	//[urls addObject:[NSURL URLWithString:@"http://static.flickr.com/90/245707482_620b878566_b.jpg"]];
 }
 
 @end
