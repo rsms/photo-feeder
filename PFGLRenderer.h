320b5d7794385f56fee6e9c8b45b18922d666600
mattias <mattias@98834e0e-571d-0410-bf09-ac0e1acfc769>
2006-10-22 00:00:26 +0000

    Removed unnecessary code + reformat
    git-svn-id: http://svn.hunch.se/photo-feeder/trunk@17 98834e0e-571d-0410-bf09-ac0e1acfc769

--- PFGLRenderer.h
+++ PFGLRenderer.h
@@ -1,6 +1,3 @@
-#import <OpenGL/gl.h>
-#import <OpenGL/glu.h>
-
 @interface PFGLRenderer : NSOpenGLView {
 	BOOL viewHasBeenReshaped;
 }
--- PFGLRenderer.m
+++ PFGLRenderer.m
@@ -1,29 +1,29 @@
-
-
 #import "PFGLRenderer.h"
 
+#import <OpenGL/gl.h>
+#import <OpenGL/glu.h>
 
 @implementation PFGLRenderer
 
-
-+ (PFGLRenderer*) newRenderer
-{
-	NSOpenGLPixelFormat *format;
++ (PFGLRenderer*) newRenderer {
+	// Setup the default renderer attributes. We don't want any fancy stuff,
+	// just the basics things for displaying images in 2D
 	NSOpenGLPixelFormatAttribute attributes[] = { 
 		NSOpenGLPFAAccelerated,
 		NSOpenGLPFANoRecovery,
-		NSOpenGLPFADepthSize, 16, // we may not need this for 2D
 		NSOpenGLPFAColorSize, 32,
-	0};
-	format = [[[NSOpenGLPixelFormat alloc] initWithAttributes:attributes] autorelease];
-	return [[[self class] alloc] initWithFrame:NSZeroRect pixelFormat:format];
+		0};
+	
+	// Setup the pixel format with the above specified attributes 
+	NSOpenGLPixelFormat *format = [[[NSOpenGLPixelFormat alloc] initWithAttributes:attributes] autorelease];
+	
+	return [[[self class] alloc] initWithFrame: NSZeroRect
+								   pixelFormat: format];
 }
 
 
-- (void)prepare
-{
+- (void)prepare {
 	[[self openGLContext] makeCurrentContext];
-	NSLog(@"[%@ prepareOpenGL] context: %x", self, [self openGLContext]);
 	
     // Enable beam-synced updates
 	long parm = 1;
@@ -32,29 +32,25 @@
 	
     // Make sure that things we don't need are disabled. Some of
 	// these are enabled by default and can slow down rendering
-    glDisable (GL_ALPHA_TEST);
-    glDisable (GL_DEPTH_TEST);
-    glDisable (GL_SCISSOR_TEST);
-    glDisable (GL_BLEND);
-    glDisable (GL_DITHER);
-    glDisable (GL_CULL_FACE);
-    glColorMask (GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
-    glDepthMask (GL_FALSE);
-    glStencilMask (0);
-    glClearColor (0.0f, 0.0f, 0.0f, 0.0f);
-    glHint (GL_TRANSFORM_HINT_APPLE, GL_FASTEST);
+    glDisable(GL_ALPHA_TEST);
+    glDisable(GL_DEPTH_TEST);
+    glDisable(GL_SCISSOR_TEST);
+    glDisable(GL_BLEND);
+    glDisable(GL_DITHER);
+    glDisable(GL_CULL_FACE);
+    glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+    glDepthMask(GL_FALSE);
+    glStencilMask(0);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glHint(GL_TRANSFORM_HINT_APPLE, GL_FASTEST);
 }
 
-
-- (void)setFrameSize:(NSSize)newSize
-{  
-	NSLog(@"-- setFrameSize");
-	[super setFrameSize:newSize];
-	//[renderer setFrameSize:newSize];
+- (void)setFrameSize:(NSSize)newSize {  
+	[super setFrameSize: newSize];
 	
 	[[self openGLContext] makeCurrentContext];
 	
-	NSRect  visibleRect = [self visibleRect];
+	NSRect visibleRect = [self visibleRect];
 	glViewport(0, 0, visibleRect.size.width, visibleRect.size.height);
 	
 	glMatrixMode(GL_PROJECTION);
@@ -67,15 +63,6 @@
 	
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	
-	
-	// Reshape 3D
-	/*glViewport( 0, 0, (GLsizei)newSize.width, (GLsizei)newSize.height );
-	glMatrixMode( GL_PROJECTION );
-	glLoadIdentity();
-	gluPerspective( 45.0f, (GLfloat)newSize.width / (GLfloat)newSize.height, 0.1f, 100.0f );
-	glMatrixMode( GL_MODELVIEW );
-	glLoadIdentity();*/
 }
 
 
