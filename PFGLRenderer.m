0d72c22b36d4e6531517893a1deb55c7bfa4324a
rasmus <rasmus@98834e0e-571d-0410-bf09-ac0e1acfc769>
2006-12-03 16:38:47 +0000

    Added GPL license
    git-svn-id: http://svn.hunch.se/photo-feeder/trunk@64 98834e0e-571d-0410-bf09-ac0e1acfc769

--- PFGLRenderer.m
+++ PFGLRenderer.m
@@ -1,3 +1,14 @@
+/*
+ * PhotoFeeder is the legal property of its developers.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version. You should have received a copy 
+ * of the GNU General Public License along with this program; if not, 
+ * write to the Free Software Foundation, Inc., 59 Temple Place, 
+ * Suite 330, Boston, MA 02111-1307 USA
+ */
 #import "PFGLRenderer.h"
 
 #import <OpenGL/gl.h>
