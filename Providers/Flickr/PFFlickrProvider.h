e4d5437686bb1d5d28043b194a34e1a230331bdc
rasmus <rasmus@98834e0e-571d-0410-bf09-ac0e1acfc769>
2007-04-13 16:17:34 +0000

    git-svn-id: http://svn.hunch.se/photo-feeder/trunk@157 98834e0e-571d-0410-bf09-ac0e1acfc769

--- Providers/Flickr/PFFlickrProvider.h
+++ Providers/Flickr/PFFlickrProvider.h
@@ -9,11 +9,13 @@
  * write to the Free Software Foundation, Inc., 59 Temple Place, 
  * Suite 330, Boston, MA 02111-1307 USA
  */
+#import <Flickr/Flickr.h>
 #import "PFProvider.h"
 #import "PFQueue.h"
 
 @interface PFFlickrProvider : PFProvider {
-	PFQueue*  urls;
+	FlickrContext*   context;
+	PFQueue*         urls;
 	NSConditionLock* activeCondLock;
 }
 
